427385220d2ead6c3eb698dd213631b94356b8f4
siddhant-afk <siddhantdudhane21@gmail.com>
2022-12-26 10:27:40 +0530

    Initial state

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+    
+
+   const date = new Date();
+   const year = date.getFullYear();
+    return <footer><p>Copyright: Siddhant Dudhane {year}</p></footer>
+}
+
+export default Footer;
