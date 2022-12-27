85c6009e0b23aab3f628dd9dbd3a8ac19c204512
siddhant-afk <siddhantdudhane21@gmail.com>
2022-12-27 11:19:38 +0530

    Add create area

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,18 @@
-import React from "react"
-import Header from "./Header"
-import Footer from "./Footer"
-import Note from "./Note"
-import notes from "../notes";
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
-
-function createNote(note){
-
-    return <Note key={note.key} title={note.title} content={note.content} />
-}
-
-function App(){
-
-    return <div>
-    <Header />
-    {notes.map(createNote)}
-    <Footer />
+function App() {
+  return (
+    <div>
+      <Header />
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
+      <Footer />
     </div>
+  );
 }
 
 export default App;
