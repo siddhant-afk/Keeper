768daf799aea745c86127b4ddd2c077ba872ec57
siddhant-afk <siddhantdudhane21@gmail.com>
2022-12-28 09:58:20 +0530

    Add hook for adding new notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  
+
+  const [notes,setNotes] = useState([]);
+
+  function addNote(note){
+
+    setNotes(prevNotes => {
+     return [...prevNotes,note]
+    });
+
+  }
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea onAdd={addNote} />
+
+      {notes.map((note)=>{
+
+        return <Note title={note.title} content={note.content} />
+      })}
+
       <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
